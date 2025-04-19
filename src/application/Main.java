488b0f2337cee6653404d7f5380576a6084e1540
Victor <luanvitorlima2017@gmail.com>
2025-04-18 22:07:48 -0300

    adicionando o primeiro post do projeto

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,8 +1,17 @@
 package application;
 
+import entities.Comment;
+import entities.Post;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
 public class Main {
-    public static void main(String[] args)
-    {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) throws ParseException {
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+        Comment c1 = new Comment("Have a nice trip!");
+        Comment c2 = new Comment("Wow that's awesome!");
+        Post p1 = new Post(sdf.parse("18/04/2025 22:05:12"), "Traveling to New Zeland", "I'm going to visit this wonderful country!", 23);
+
     }
 }
